1aaf5062beac070ae21e3b3aae81524cbd7d1415
Jordan Lane <jlane@ualberta.ca>
2019-03-18 15:59:44 -0600

    WinCheck Tests
    * OT and RedYellow WinCheck tests
    
    * Fix test requirements

---
+++ lib/models/yellow_win_check.rb
@@ -0,0 +1,11 @@
+module YellowWinCheck
+  extend self
+
+  def is_winner?(board)
+    raise NotImplementedError
+  end
+
+  def get_winner(board)
+    raise NotImplementedError
+  end
+end
---
+++ test/win_check_test.rb
@@ -0,0 +1,236 @@
+require 'test/unit'
+require_relative '../lib/models/game_board'
+require_relative '../lib/models/counter'
+require_relative '../lib/models/otto_win_check'
+require_relative '../lib/models/toot_win_check'
+require_relative '../lib/models/red_win_check'
+require_relative '../lib/models/yellow_win_check'
+
+class WinCheckTest < Test::Unit::TestCase
+  TEST_ITER = 10
+
+  def _setup
+    @o_counter = Counter.new("O", "../TODO/")
+    @t_counter = Counter.new("T", "../TODO/")
+
+    @red_counter = Counter.new("R", "../TODO/")
+    @yellow_counter = Counter.new("Y", "../TODO/")
+
+  end
+
+  def teardown; end
+
+  def board_counter_check(board, valid_counters)
+    # TODO: Check that all counters on the board are in the list of valid_counters
+    return TRUE
+  end
+
+  def generate_red_yellow_win_boards(winning_counter, losing_counter)
+    board_list = []
+
+    horiz_win_board = GameBoard.new(5)
+    horiz_win_board.place(winning_counter, 1)
+    horiz_win_board.place(winning_counter, 2)
+    horiz_win_board.place(winning_counter, 3)
+    horiz_win_board.place(winning_counter, 4)
+    board_list.push(horiz_win_board)
+
+    vert_win_board = GameBoard.new(5)
+    vert_win_board.place(winning_counter, 1)
+    vert_win_board.place(winning_counter, 1)
+    vert_win_board.place(winning_counter, 1)
+    vert_win_board.place(winning_counter, 1)
+    board_list.push(vert_win_board)
+
+    diag_win_board = GameBoard.new( 5)
+    diag_win_board.place(winning_counter, 1)
+    diag_win_board.place(losing_counter, 2)
+    diag_win_board.place(winning_counter, 2)
+    diag_win_board.place(losing_counter, 3)
+    diag_win_board.place(losing_counter, 3)
+    diag_win_board.place(winning_counter, 3)
+    diag_win_board.place(losing_counter, 4)
+    diag_win_board.place(losing_counter, 4)
+    diag_win_board.place(losing_counter, 4)
+    diag_win_board.place(winning_counter, 4)
+    board_list.push(diag_win_board)
+
+    return board_list
+  end
+
+  def generate_otto_win_boards
+    board_list = []
+
+    horiz_win_board = GameBoard.new(5)
+    horiz_win_board.place(@o_counter, 1)
+    horiz_win_board.place(@t_counter, 2)
+    horiz_win_board.place(@t_counter, 3)
+    horiz_win_board.place(@o_counter, 4)
+    board_list.push(horiz_win_board)
+
+    vert_win_board = GameBoard.new(5)
+    vert_win_board.place(@o_counter, 1)
+    vert_win_board.place(@t_counter, 1)
+    vert_win_board.place(@t_counter, 1)
+    vert_win_board.place(@o_counter, 1)
+    board_list.push(vert_win_board)
+
+    return board_list
+  end
+
+  def generate_toot_win_boards
+    board_list = []
+
+    horiz_win_board = GameBoard.new(5)
+    horiz_win_board.place(@t_counter, 1)
+    horiz_win_board.place(@o_counter, 2)
+    horiz_win_board.place(@o_counter, 3)
+    horiz_win_board.place(@t_counter, 4)
+    board_list.push(horiz_win_board)
+
+    vert_win_board = GameBoard.new(5)
+    vert_win_board.place(@t_counter, 1)
+    vert_win_board.place(@o_counter, 1)
+    vert_win_board.place(@o_counter, 1)
+    vert_win_board.place(@t_counter, 1)
+    board_list.push(vert_win_board)
+
+    return board_list
+  end
+
+  #### TESTING ####
+
+  def tst_ot_neutral
+    ot_neutral_board = GameBoard.new(5)
+    ot_neutral_board.place(@o_counter, 1)
+    ot_neutral_board.place(@t_counter, 4)
+
+    # Preconditions
+    begin
+      assert_true( ot_neutral_board.is_a?(GameBoard), "Board is not of type GameBoard")
+      assert_true( board_counter_check(ot_neutral_board, [@o_counter, @t_counter]), "GameBoard contains invalid counter types")
+    end
+
+    otto_result = OttoWinCheck.new().is_winner?(ot_neutral_board)
+    toot_result = TootWinCheck.new().is_winner?(ot_neutral_board)
+
+    # Postconditions
+    begin
+      assert_false(otto_result, "OTTO win check incorrectly found a win on a neutral board")
+      assert_false(toot_result, "TOOT win check incorrectly found a win on a neutral board")
+    end
+  end
+
+  def tst_otto_win
+    otto_win_boards = generate_otto_win_boards()
+
+    otto_win_boards.each do |otto_win_board|
+      # Preconditions
+      begin
+        assert_true( otto_win_board.is_a?(GameBoard), "Board is not of type GameBoard")
+        assert_true( board_counter_check(otto_win_board, [@o_counter, @t_counter]), "GameBoard contains invalid counter types")
+      end
+
+      otto_result = OttoWinCheck.new().is_winner?(otto_win_board)
+      toot_result = TootWinCheck.new().is_winner?(otto_win_board)
+
+      # Postconditions
+      begin
+        assert_true(otto_result, "OTTO missed win on a winning board")
+        assert_false(toot_result, "TOOT incorrectly found a win on a losing board")
+      end
+    end
+  end
+
+
+  def tst_toot_win
+    toot_win_boards = generate_otto_win_boards()
+
+    toot_win_boards.each do |toot_win_board|
+
+      # Preconditions
+      begin
+        assert_true( toot_win_board.is_a?(GameBoard), "Board is not of type GameBoard")
+        assert_true( board_counter_check(toot_win_board, [@o_counter, @t_counter]), "GameBoard contains invalid counter types")
+      end
+
+      otto_result = OttoWinCheck.new().is_winner?(toot_win_board)
+      toot_result = TootWinCheck.new().is_winner?(toot_win_board)
+
+      # Postconditions
+      begin
+        assert_false(otto_result, "OTTO incorrectly found a win on a losing board")
+        assert_true(toot_result, "TOOT missed win on a winning board")
+      end
+    end
+  end
+
+
+  def tst_red_yellow_neutral
+    red_yellow_neutral_board = GameBoard.new(5)
+    red_yellow_neutral_board.place(@red_counter, 1)
+    red_yellow_neutral_board.place(@yellow_counter, 1)
+
+    # Preconditions
+    begin
+      assert_true( red_yellow_neutral_board.is_a?(GameBoard), "Board is not of type GameBoard")
+      assert_true( board_counter_check(red_yellow_neutral_board, [@red_counter, @yellow_counter]), "GameBoard contains invalid counter types")
+    end
+
+    red_result = RedWinCheck.new().is_winner?(red_yellow_neutral_board)
+    yellow_result = YellowWinCheck.new().is_winner?(red_yellow_neutral_board)
+
+    # Postconditions
+    begin
+      assert_false(red_result, "RED win check incorrectly found a win on a neutral board")
+      assert_false(yellow_result, "YELLOW win check incorrectly found a win on a neutral board")
+    end
+  end
+
+
+  def tst_red_win
+    red_win_boards = generate_red_yellow_win_boards(@red_counter, @yellow_counter)
+
+    red_win_boards.each do |red_win_board|
+
+      # Preconditions
+      begin
+        assert_true( red_win_board.is_a?(GameBoard), "Board is not of type GameBoard")
+        assert_true( board_counter_check(red_win_board, [@red_counter, @yellow_counter]), "GameBoard contains invalid counter types")
+      end
+
+      red_result = RedWinCheck.new().is_winner?(red_win_board)
+      yellow_result = YellowWinCheck.new().is_winner?(red_win_board)
+
+      # Postconditions
+      begin
+        assert_true(red_result, "RED win check missed win on a winning board")
+        assert_false(yellow_result, "YELLOW win check incorrectly found a win on a losing board")
+      end
+    end
+
+  end
+
+
+  def tst_yellow_win
+    yellow_win_boards = generate_red_yellow_win_boards(@yellow_counter, @red_counter)
+
+    yellow_win_boards.each do |yellow_win_board|
+      # Preconditions
+      begin
+        assert_true( yellow_win_board.is_a?(GameBoard), "Board is not of type GameBoard")
+        assert_true( board_counter_check(yellow_win_board, [@red_counter, @yellow_counter]), "GameBoard contains invalid counter types")
+      end
+
+      red_result = RedWinCheck.new().is_winner?(yellow_win_board)
+      yellow_result = YellowWinCheck.new().is_winner?(yellow_win_board)
+
+      # Postconditions
+      begin
+        assert_false(red_result, "RED win check incorrectly found a win on a losing board")
+        assert_true(yellow_result, "YELLOW win check missed win on a winning board")
+      end
+    end
+  end
+
+end
