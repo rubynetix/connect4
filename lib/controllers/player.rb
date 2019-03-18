304269c9849166960a06ce05fbfb38ad9f3fc890
LionKingSimba <ttran1@ualberta.ca>
2019-03-18 16:25:18 -0600

    player contract

--- lib/controllers/player.rb
+++ lib/controllers/player.rb
@@ -3,7 +3,22 @@ class Player
     @name = name
   end
 
-  def take_turn(board)
+  def take_turn(board, ui)
+    ui.register(self)
+    raise NotImplementedError
+  end
+
+  def register(ui, event_filter)
+    @event_filter = event_filter
+    ui.register(self)
+    raise NotImplementedError
+  end
+
+  def notify(event)
+    raise NotImplementedError
+  end
+
+  def get_action
     raise NotImplementedError
   end
 end
---
+++ test/player_test.rb
@@ -0,0 +1,32 @@
+require 'test/unit'
+require_relative '../lib/controllers/player'
+
+class PlayerTest < Test::Unit::TestCase
+  TEST_ITER = 10
+
+  def setup
+    @player = Player.new Tester
+  end
+
+  def teardown; end
+
+  def initialize;  end
+
+  def tst_take_turn
+    # Preconditions
+      # None
+
+    board = GameBoard.new
+    ui = MockUi.new
+    mockbutton = MockButton.new
+    ui.add_button(mockbutton)
+    @player.register(nil, ui)
+    action = @player.take_turn(board, ui)
+
+    # Simulate button click
+
+    # Postconditions
+      # A PlayerAction is returned
+    assert(action.is_a? PlayerAction)
+  end
+end
