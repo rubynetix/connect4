f7d47a8c949ab325b041cd5cb5404e05867327b6
LionKingSimba <ttran1@ualberta.ca>
2019-03-22 16:32:19 -0600

    connect4 loop

--- lib/models/connect4.rb
+++ lib/models/connect4.rb
@@ -1,15 +1,26 @@
 require_relative 'game_stats'
 require_relative '../view/UI'
+require_relative '../controllers/local_player'
+require_relative '../controllers/game'
+require_relative '../models/game_board'
+require_relative '../models/win_check'
 
 class Connect4
   include GameStats
   def initialize(ui, stats)
     @ui = ui
     @game_stats = GameStats.new stats
+    @players = []
+    @gameboard = GameBoard.new
+    @win_check = WinCheck.new 'YYYY', 'RRRR'
   end
 
   def app_loop
-    raise(NotImplementedError)
+    # TODO: Implement a main menu
+    @players [LocalPlayer.new, LocalPlayer.new]
+    loop do
+      launch_game @players, @gameboard, @win_check
+    end
   end
 
   def load_stats
@@ -20,8 +31,8 @@ class Connect4
     raise(NotImplementedError)
   end
 
-  def launch_game(players)
-    raise(NotImplementedError)
+  def launch_game
+    Game.new @players, @gameboard, @win_check
   end
 
   def update_game_stats(update)
