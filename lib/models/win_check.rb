d832f407fdf139533b5881890e3d5bef26f8b228
Ryan Furrer <rfurrer@ualberta.ca>
2019-03-23 17:15:21 -0600

    images

--- lib/models/win_check.rb
+++ lib/models/win_check.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'singleton'
 
 module WinEnum
@@ -8,41 +10,79 @@ module WinEnum
 end
 
 class WinCheck
-
   def initialize(win_string1, win_string2)
+    @win_strings = [win_string1, win_string2]
     @win1_string = win_string1
     @win2_string = win_string2
   end
 
-
   def check(board)
-    win1 = false
-    win2 = false
+    winners = []
 
-    create_strings(board, board.last_counter_pos).each do | string |
-      win1 = win1 or string.include? @win1_string
-      win2 = win2 or string.include? @win2_string
+    create_strings(board).each do |string|
+      @win_strings.each do |player|
+        winners.push(player) if string.include? player
+      end
     end
 
-    if win1 and win2 #or board.full?
-      result = WinEnum::DRAW
-    elsif win1
-      result = WinEnum::WIN1
-    elsif win2
-      result = WinEnum::WIN2
-    else
-      result = WinEnum::NEUTRAL
-    end
+    result = if winners.size > 1
+               WinEnum::DRAW
+             elsif winners.size.zero? # or board.full?
+               WinEnum::NEUTRAL
+             elsif winners[0] == @win1_string
+               WinEnum::WIN1
+             else
+               WinEnum::WIN2
+             end
 
-    return result
+    result
   end
 
   private
 
-  def create_strings(board, position)
-    row = position[0]
-    col = position[1]
-    return []
+  def col_string(board, x, y1, y2)
+    s = ''
+    (y1..y2).each { |y| s += board.at(x, y) }
+    s
   end
 
+  def row_string(board, y, x1, x2)
+    s = ''
+    (x1..x2).each { |x| s += board.at(x, y) }
+    s
+  end
+
+  def dia_string(board, min_x, center_x, max_x, max_y, xy_diff)
+    up = ''
+    (min_x..center_x).each { |x| up += board.at(x, x - xy_diff) if x - xy_diff >= 0 }
+    (center_x + 1..max_x).each { |x| up += board.at(x, x - xy_diff) if x - xy_diff <= max_y }
+    down = ''
+    (min_x..center_x).each do |x|
+      y = center_x * 2 - x - xy_diff
+      down += board.at(x, y) if y >= 0
+    end
+    (center_x + 1..max_x).each do |x|
+      y = center_x * 2 - x - xy_diff
+      down += board.at(x, y) if y <= max_y
+    end
+    [up, down]
+  end
+
+  def create_strings(board)
+    x, y = board.last_counter_pos
+
+    max_offset = @win_strings.map(&:size).max
+    min_x = [x - max_offset, 0].max
+    min_y = [y - max_offset, 0].max
+    max_x = [x + max_offset, board.rows].max
+    max_y = [y + max_offset, board.cols].max
+
+    possible_win_area = []
+    possible_win_area.push(col_string(board, x, min_y, max_y))
+    possible_win_area.push(row_string(board, y, min_x, max_x))
+    dia_string(board, min_x, x, max_x, max_y, x - y).each do |string|
+      possible_win_area.push(string)
+    end
+    possible_win_area
+  end
 end
