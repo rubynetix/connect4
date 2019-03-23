ec337463fe5bac068b26f26640eecd9ae7c44ffe
LionKingSimba <ttran1@ualberta.ca>
2019-03-23 15:37:20 -0600

    player test_forfeit and test_counter

--- test/player_test.rb
+++ test/player_test.rb
@@ -1,30 +1,49 @@
 require 'test/unit'
 require_relative '../lib/controllers/player'
+require_relative '../lib/controllers/player_action'
+require_relative '../lib/models/counter'
+require_relative 'mock/mock_event'
+require_relative '../lib/views/events/forfeit_click_event'
+require_relative '../lib/views/events/cell_click_event'
+require_relative 'mock/mock_ui'
 
 class PlayerTest < Test::Unit::TestCase
   TEST_ITER = 10
 
   def setup
-    @player = Player.new Tester
+    @player = Player.new("Tester", YellowCounter.instance)
   end
 
   def teardown; end
 
-  def tst_take_turn
+  def test_take_turn_forfeit
     # Preconditions
-      # None
+    #  - None
 
     board = GameBoard.new
-    ui = MockUi.new
-    mockbutton = MockButton.new
-    ui.add_button(mockbutton)
-    @player.register(nil, ui)
-    action = @player.take_turn(board, ui)
-
-    # Simulate button click
+    ui = MockUI.new
+    @player.register(ui, [ForfeitClickEvent])
+    action = Thread.new { @player.take_turn(board, ui) }
+    ui.notify_all(ForfeitClickEvent.new) # Simulate button click
 
     # Postconditions
-      # A PlayerAction is returned
-    assert(action.is_a? PlayerAction)
+    #  - A Forfeit Action is returned
+    assert(action.value == PlayerAction::FORFEIT)
+  end
+
+  def test_take_turn_counter
+    # Preconditions
+    #  - None
+
+    board = GameBoard.new
+    ui = MockUI.new
+    @player.register(ui, [CellClickEvent])
+    action = Thread.new { @player.take_turn(board, ui) }
+    ui.notify_all(CellClickEvent.new(0, 0)) # Simulate button click
+
+    # Postconditions
+    #  - A Place Counter Action is returned
+    # .- The board now has a cell at location x y
+    assert(action.value == PlayerAction::PLACE_COUNTER)
   end
 end
