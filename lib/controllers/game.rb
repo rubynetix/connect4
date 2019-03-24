cb86ec946f2d82b6c3f028778fdb211e96bbcd9a
James Hryniw <jhryniw@gmail.com>
2019-03-24 15:59:47 -0600

    forfeits (#53)

--- lib/controllers/game.rb
+++ lib/controllers/game.rb
@@ -1,5 +1,7 @@
 class Game
   def initialize(config)
+    raise NotTwoPlayersError unless config.players.size == 2
+
     @players = config.players
     @gameboard = config.gameboard
     @win_check = config.win_check
@@ -12,8 +14,8 @@ class Game
     update_board
     until @done
       @players.each do |p|
-        process_action(p.take_turn(@gameboard, @ui))
-        process_state
+        p_action = p.take_turn(@gameboard, @ui)
+        process_action(p, p_action)
         update_board
         break if @done
       end
@@ -23,20 +25,22 @@ class Game
 
   private
 
-  def process_action(action)
-    @done = action == PlayerAction::FORFEIT
-  end
-
-  def process_state
-    case @win_check.check(@gameboard)
-    when WinEnum::DRAW
+  def process_action(player, action)
+    case action
+    when PlayerAction::FORFEIT
       @done = true
-    when WinEnum::WIN1
-      @done = true
-      @winner = @players[0]
-    when WinEnum::WIN2
-      @done = true
-      @winner = @players[1]
+      @winner = other_player(player)
+    when PlayerAction::PLACE_COUNTER
+      case @win_check.check(@gameboard)
+      when WinEnum::DRAW
+        @done = true
+      when WinEnum::WIN1
+        @done = true
+        @winner = @players[0]
+      when WinEnum::WIN2
+        @done = true
+        @winner = @players[1]
+      end
     end
   end
 
@@ -45,4 +49,15 @@ class Game
     puts @gameboard.to_s
     @ui.draw_gameboard(@gameboard)
   end
+
+  def other_player(p)
+    case p
+    when @players[0]
+      @players[1]
+    else
+      @players[0]
+    end
+  end
 end
+
+class NotTwoPlayersError < StandardError; end
