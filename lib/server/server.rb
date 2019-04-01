243e15f55e981e22fa7ed7e03f5901bd171d80da
LionKingSimba <ttran1@ualberta.ca>
2019-04-01 14:21:30 -0600

    Xml rpc (#65)
    * server using xml rpc
    
    * add server test file

---
+++ lib/server/server.rb
@@ -0,0 +1,31 @@
+require 'xmlrpc/server'
+
+# Handles user related requests
+class UserHandler
+  # Returns games of a user
+  def games(username)
+    { 'games' => ['game1', 'game2'] }
+  end
+end
+
+# Handles game related requests
+class GameHandler
+  # Creates a new game
+  def create(username1, username2)
+    { 'game' => ['gamename', username1, username2] }
+  end
+end
+
+# Handles league related requests
+class LeagueHandler
+  # Returns league standings of a user
+  def standings(username)
+    { 'standings' => ['wins', 'losses'] }
+  end
+end
+
+s = XMLRPC::Server.new(8080)
+s.add_handler('user', UserHandler.new)
+s.add_handler('game', GameHandler.new)
+s.add_handler('league', LeagueHandler.new)
+s.serve
---
+++ test/server_test.rb
@@ -0,0 +1,43 @@
+require 'test/unit'
+require_relative '../lib/server/server'
+
+class ServerTest < Test::Unit::TestCase
+  TEST_ITER = 10
+
+  def setup
+    @server = XMLRPC::Client.new('localhost', '/', 8080)
+  end
+
+  def teardown; end
+
+  def tst_user_games
+    # Preconditions
+    begin
+    end
+
+    # Postconditions
+    begin
+    end
+  end
+
+  def tst_game_create
+    # Preconditions
+    begin
+    end
+
+    # Postconditions
+    begin
+    end
+  end
+
+  def tst_league_standings
+    # Preconditions
+    begin
+    end
+
+    # Postconditions
+    begin
+    end
+  end
+
+end
