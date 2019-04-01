6baec0691797df0737de4671af7416a84d5691d4
Fraser Bulbuc <fraserbulbuc@gmail.com>
2019-04-01 16:53:29 -0600

    Comment out serve

--- lib/server/server.rb
+++ lib/server/server.rb
@@ -24,8 +24,8 @@ class LeagueHandler
   end
 end
 
-s = XMLRPC::Server.new(8080)
-s.add_handler('user', UserHandler.new)
-s.add_handler('game', GameHandler.new)
-s.add_handler('league', LeagueHandler.new)
-s.serve
+# s = XMLRPC::Server.new(8080)
+# s.add_handler('user', UserHandler.new)
+# s.add_handler('game', GameHandler.new)
+# s.add_handler('league', LeagueHandler.new)
+# s.serve
