7fd9a20a063b8759f223bdc842079aea6711b90c
LionKingSimba <ttran1@ualberta.ca>
2019-04-05 14:10:42 -0600

    Tim base_handler updates (#78)
    * user query prepare in base_handler
    
    * add query function to base_hander

--- server/base_handler.rb
+++ server/base_handler.rb
@@ -12,17 +12,20 @@ class BaseHandler
   end
 
   def user_exists?(username)
-    puts "SELECT * FROM users WHERE username='#{username}'"
-    r = @db_client.query("SELECT * FROM users WHERE username='#{username}'")
+    r = query("SELECT * FROM users WHERE username=?", username, :symbolize_keys => true)
     r.count > 0
   end
 
   def get_user(username)
-    raise UserDoesNotExist unless user_exists?
-    puts "SELECT * FROM users WHERE username='#{username}'"
-    r = @db_client.query("SELECT * FROM users WHERE username='#{username}'",
-                     :symbolize_keys => true)
+    raise UserDoesNotExist unless user_exists? username
+    r = query("SELECT * FROM users WHERE username=?", username, :symbolize_keys => true)
     r[:username]
+  end
+
+  def query(sql, *args, **kwargs)
+    @db_client.prepare(sql)
+    r = query.execute(*args, **kwargs)
+    r
   end
 
   def transaction
