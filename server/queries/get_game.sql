d22e9fca0edd58881e04893fa5ec24d6ab30d3fa
jhryniw <jhryniw@gmail.com>
2019-04-11 15:51:24 -0600

    put fixes

--- server/queries/get_game.sql
+++ server/queries/get_game.sql
@@ -1,5 +1,5 @@
-SELECT g.game_id, state, turn, board
+SELECT BIN_TO_UUID(g.game_id) as game_id, state, turn, board
 FROM games g
   JOIN game_boards gb
   ON g.game_id = gb.game_id
-WHERE game_id=UUID_TO_BIN(?);
+WHERE g.game_id=UUID_TO_BIN(?);
--- server/queries/update_board.sql
+++ server/queries/update_board.sql
@@ -1,2 +1,2 @@
-UPDATE games SET board = ?
+UPDATE game_boards SET board = ?
 WHERE game_id = UUID_TO_BIN(?);
