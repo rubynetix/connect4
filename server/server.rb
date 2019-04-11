8ff81447fbef256c414e632d13d7185f2a82b573
Fraser Bulbuc <fraserbulbuc@gmail.com>
2019-04-10 18:49:49 -0600

    Cybera server (#109)
    * Help and error dialogs
    
    * Lab debug window
    
    * Lab debug, fix css error
    
    * Remove overlay
    
    * Overlay working on lab
    
    * Remove temp
    
    * Remove debug window
    
    * Remove debug require
    
    * Cybera server set up

--- server/server.rb
+++ server/server.rb
@@ -3,8 +3,8 @@ require_relative 'game_handler'
 require_relative 'league_handler'
 require_relative 'user_handler'
 
-def serve(port: 8080)
-  s = XMLRPC::Server.new(port)
+def serve(port: 8080, host: nil)
+  s = host.nil? ? XMLRPC::Server.new(port) : XMLRPC::Server.new(port, host)
   s.add_handler('user', UserHandler.new)
   s.add_handler('game', GameHandler.new)
   s.add_handler('league', LeagueHandler.new)
--- server/start.rb
+++ server/start.rb
@@ -1,9 +1,3 @@
 require_relative 'server'
-require_relative 'user_handler'
-require_relative 'game_handler'
 
-s = XMLRPC::Server.new(8080)
-s.add_handler('user', UserHandler.new)
-s.add_handler('game', GameHandler.new)
-s.add_handler('league', LeagueHandler.new)
-s.serve
+serve(port: 8080, host: "")
