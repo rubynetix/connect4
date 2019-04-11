6995a1ed1b707cbf1de526df32068fefec1aae6b
Ryan Furrer <rfurrer@ualberta.ca>
2019-04-11 17:58:10 -0600

    Work for tests

--- test/client_test.rb
+++ test/client_test.rb
@@ -1,10 +1,26 @@
 require_relative 'helper'
 require_relative '../server/server'
+require_relative '../client/models/game_board'
 
 class ClientTest < Helper
 
   def setup_db
-    #
+    @tdbh = TestDBHandler.new
+  end
+
+  def rand_board(fill_factor: rand(1..100))
+    board = GameBoard.connect4
+    counters = ([fill_factor.abs.to_f, 1.0].min * board.rows * board.cols).floor
+
+    while counters > 0
+      c = rand(0...board.cols)
+      unless board.col_full?(c)
+        board.place(@default_counter, c)
+        counters -= 1
+      end
+    end
+
+    board
   end
 
   def setup
@@ -12,6 +28,7 @@ class ClientTest < Helper
     @server_thread = Thread.new do
       serve
     end
+    @client = Client.new
   end
 
   def teardown
@@ -19,22 +36,47 @@ class ClientTest < Helper
   end
 
   def test_user_games
-    user = 'user_games'
+    user = @tdbh.users.sample
+    length = 3
 
+    gids = @client.user_games user
+
+    begin
+      assert_equal gids.length, length
+    end
   end
 
   def test_create_game
-    user = 'create_game'
+    user1 = @tdbh.users.sample
+    @tdbh.users.delete(user1)
+    user2 = @tdbh.users.sample
+    gids1 = @client.user_games user1
+    gids2 = @client.user_games user1
+
+
+    gid = @client.create_game user1, user2, 'connect4', rand_board
+
+    new_gids1 = @client.user_games user1
+    new_gids2 = @client.user_games user1
+
+    begin
+      assert_equal gids1+1, new_gids1
+      assert_equal gids2+1, new_gids2
+      assert_not_include gids1, gid
+      assert_not_include gids2, gid
+      assert_include new_gids1, gid
+      assert_include new_gids2, gid
+    end
 
   end
 
   def test_get_game
-    user = 'get_game'
+    user = @tdbh.users.sample
 
   end
 
   def test_put_game
-    user = 'put_game'
+    user = @tdbh.users.sample
 
   end
 end
--- test/db_test/create_test_db.rb
+++ test/db_test/create_test_db.rb
@@ -7,19 +7,37 @@ require_relative '../../client/models/game_board'
 
 class TestDBHandler < BaseHandler
 
+  attr_reader :users
+
   def initialize
     super
     @uuid = UUID.new
     @game_uuids = []
+    @users = ['conservative', 'ndp', 'green', 'rhinoceros', 'liberals']
     query("DELETE from games;")
     query("DELETE from users;")
     load_users
     load_games
   end
 
+  def rand_board(fill_factor: rand(1..100))
+    board = GameBoard.connect4
+    counters = ([fill_factor.abs.to_f, 1.0].min * board.rows * board.cols).floor
+
+    while counters > 0
+      c = rand(0...board.cols)
+      unless board.col_full?(c)
+        board.place(@default_counter, c)
+        counters -= 1
+      end
+    end
+
+    board
+  end
+
   def load_users
-    File.open(Dir.pwd + '/test/db_test/queries/create_test_db_users.sql').each do |line|
-      query(line)
+    @users.each do |user|
+      query('INSERT INTO users (username) VALUES (?);', user)
     end
   end
 
